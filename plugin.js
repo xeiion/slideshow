0d5ac9149db48acc1f776edcac302b858debd8bc
xeiion <jackkalle@hotmail.com>
2015-05-13 04:32:03 +0200

    Converted to plugin

---
+++ plugin.js
@@ -0,0 +1,103 @@
+(function ($) {
+
+    $.fn.xeiion_slide = function (options) {
+
+        var settings = $.extend({
+        }, options);
+
+        var ProgressTimer = 0;
+        var Count = 0;
+        var Container = $('#slideShow');
+        var listItems = Container.children('.item');
+        progress = '';
+        SlideTimer = '';
+        NewCount = '';
+
+
+        $('.item').each(function (i) {
+            $('.Slideshow-Controls ul').append('<li><a data-slide-to="' + (i + 1) + '" href="">' + (i + 1) + '</a></li>');
+        });
+
+
+        $('.Slideshow-Controls ul li a').click(function (event) {
+            event.preventDefault();
+            $('#slideShow .item').hide().removeClass('active').css('opacity', '0');
+            NewCount = $(this).attr('data-slide-to');
+            progress.stop();
+            clearInterval(ProgressTimer);
+            $('.progressbar').css('width', '0');
+            Progressbar();
+            $("#slideShow .item:nth-child(" + NewCount + ")").show().addClass('active').animate({'opacity': '1'}, settings.Pageload);
+
+            stopslider();
+            startslider();
+            Progressbar();
+        });
+
+
+        function startslider() {
+            if ($('#slideShow').attr('data')) {
+                Count = $('#slideShow').attr('data');
+            } else {
+                Count = NewCount;
+            }
+            SlideTimer = setInterval(function () {
+                   $('#slideShow').removeAttr('data');
+                Count++;
+                if (Count === Container.children().length + 1) {
+                    Count = 1;
+                }
+
+                $('#slideShow .item').css('opacity', '0').removeClass('active').hide();
+                $('#slideShow .item:nth-child(' + Count + '').show().addClass('active').animate({'opacity': '1'}, settings.Pageload);
+            }, settings.Pageload);
+        }
+
+
+        function stopslider() {
+            clearInterval(ProgressTimer);
+            clearInterval(SlideTimer);
+        }
+
+
+        function Progressbar() {
+            if (settings.autoplay) {
+                ProgressTimer = setInterval(function () {
+
+                    totaltWidth = $(window).width();
+                    if ($('.progressbar').width() === totaltWidth) {
+                        $('.progressbar').css('width', '0%');
+                    }
+
+                    progress = $('.progressbar').animate({'width': '100%'}, settings.Pageload - 20);
+                }, settings.Pageload);
+            }
+        }
+
+
+        $('#slideShow').mouseover(function () {
+            if (progress) {
+                progress.stop();
+            }
+            stopslider();
+        });
+
+
+        $('#slideShow').mouseout(function () {
+            Count = $('#slideShow').attr('data', Count);
+            width = $('.progressbar').width();
+
+            startslider();
+            Progressbar();
+        });
+
+
+        Progressbar();
+
+        listItems.not(':first').hide();
+        if (settings.autoplay) {
+            startslider();
+        }
+    };
+
+}(jQuery));
